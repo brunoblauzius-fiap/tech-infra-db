ca884573f645a191a8dd911d3c5faa20b7613a66
Bruno Blauzius Schuindt <bruno.blauzius@lojacorr.local>
2024-03-12 15:11:11 -0300

    statefullsets

--- terraform_state.tf
+++ terraform_state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-clickmark-tf"
-    key    = "terraform-clickmark-tf/terraform.tfstate"
+    key    = "terraform-infra-db/terraform.tfstate"
     region = "us-east-1"
   }
 }
